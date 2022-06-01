a2f91e7af534ec867d1256ec7933a902e8864df8
Falko Schumann <falko.schumann@muspellheim.de>
2022-06-01 22:40:52 +0200

    Configure Flow and Prettier

--- todos-frontend/src/App.js
+++ todos-frontend/src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+// @flow
 
-function App() {
+import './App.css';
+import logo from './logo.svg';
+
+function App(): React$Element<any> {
   return (
     <div className="App">
       <header className="App-header">
--- todos-frontend/src/App.test.js
+++ todos-frontend/src/App.test.js
@@ -1,3 +1,5 @@
+// @flow
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
