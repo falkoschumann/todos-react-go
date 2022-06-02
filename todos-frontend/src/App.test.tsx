6f14c6d0e65c364984df2b95397ef47595b178e6
Falko Schumann <falko.schumann@muspellheim.de>
2022-06-02 21:57:06 +0200

    Move CRA from Flow to TypeScript

--- todos-frontend/src/App.test.tsx
+++ todos-frontend/src/App.test.tsx
@@ -1,5 +1,4 @@
-// @flow
-
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
--- todos-frontend/src/App.tsx
+++ todos-frontend/src/App.tsx
@@ -1,15 +1,14 @@
-// @flow
-
-import './App.css';
+import React from 'react';
 import logo from './logo.svg';
+import './App.css';
 
-function App(): React$Element<any> {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
