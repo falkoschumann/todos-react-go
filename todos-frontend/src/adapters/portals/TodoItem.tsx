7ed4722d9623402c4b94b09d8f9b9872d09b685a
Falko Schumann <falko.schumann@muspellheim.de>
2022-07-04 20:37:24 +0200

    Fix markup

--- todos-frontend/src/adapters/portals/TodoItem.tsx
+++ todos-frontend/src/adapters/portals/TodoItem.tsx
@@ -35,12 +35,8 @@ function TodoItem({
 
   function handleSubmit() {
     const value = editText.trim();
-    if (value) {
-      onSave?.(value);
-      setEditText(value);
-    } else {
-      onDestroy?.();
-    }
+    onSave?.(value);
+    setEditText(value);
   }
 
   function handleKeyDown(event: KeyboardEvent<HTMLInputElement>) {
@@ -71,14 +67,16 @@ function TodoItem({
   }, [editing, prevEditing]);
 
   return editing ? (
-    <input
-      ref={editorRef}
-      className="w-full p-4 pl-16 font-light text-2xl shadow-inner focus:outline-red-700/40"
-      value={editText}
-      onBlur={handleSubmit}
-      onChange={handleChange}
-      onKeyDown={handleKeyDown}
-    />
+    <li className="relative text-2xl border-b border-solid border-gray-200 last:border-b-0">
+      <input
+        ref={editorRef}
+        className="w-full p-4 pl-16 font-light text-2xl shadow-inner focus:outline-red-700/40"
+        value={editText}
+        onBlur={handleSubmit}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+    </li>
   ) : (
     <li className="group relative text-2xl border-b border-solid border-gray-200 last:border-b-0">
       <input
